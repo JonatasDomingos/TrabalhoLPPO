e80460a8b4bee177371cfa274273a5bae2f66de4
DomingosJ <jonatascarvalhodomingos@hotmail.com>
2020-11-25 16:10:54 -0300

    Trabalho

---
+++ build/generated-sources/ap-source-output/dominio/Aluno_.java
@@ -0,0 +1,18 @@
+package dominio;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Aluno.class)
+public abstract class Aluno_ {
+
+	public static volatile SingularAttribute<Aluno, String> pcd;
+	public static volatile SingularAttribute<Aluno, String> nascimento;
+	public static volatile SingularAttribute<Aluno, String> nome;
+	public static volatile SingularAttribute<Aluno, Long> id;
+	public static volatile SingularAttribute<Aluno, String> turma;
+
+}
+
---
+++ build/generated-sources/ap-source-output/dominio/Turma_.java
@@ -0,0 +1,18 @@
+package dominio;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Turma.class)
+public abstract class Turma_ {
+
+	public static volatile SingularAttribute<Turma, String> ano;
+	public static volatile SingularAttribute<Turma, String> nome;
+	public static volatile SingularAttribute<Turma, Long> id;
+	public static volatile SingularAttribute<Turma, String> ensino;
+	public static volatile SingularAttribute<Turma, String> totalAlunos;
+
+}
+
